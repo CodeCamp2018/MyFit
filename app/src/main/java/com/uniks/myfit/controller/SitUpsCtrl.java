9102b52be761d56c8d230484e071ae9784f143d6
CodeCamp2018 <41893606+CodeCamp2018@users.noreply.github.com>
2018-08-02 13:51:08 +0200

    Merge branch 'master' of https://github.com/CodeCamp2018/MyFit
    # Conflicts:
    #	app/src/main/java/com/uniks/myfit/TrackingViewActivity.java

--- app/src/main/java/com/uniks/myfit/controller/SitUpsCtrl.java
+++ app/src/main/java/com/uniks/myfit/controller/SitUpsCtrl.java
@@ -2,24 +2,30 @@ package com.uniks.myfit.controller;
 import com.uniks.myfit.Accelerometer;
 import com.uniks.myfit.Gyroscope;
 import com.uniks.myfit.TrackingViewActivity;
+import com.uniks.myfit.model.AccTriple;
 
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+
+import java.util.Iterator;
 import java.util.List;
 
 public class SitUpsCtrl {
 
     private int situpCount; // situp counter
-    public static final String ACC_THRESHOLD_MIN_VALUE = "threshold_min_value";
-    public static final String ACC_THRESHOLD_MAX_VALUE = "threshold_max_value";
+    public static final String  rising = "found";
+    public static final String falling = "found";
     Accelerometer accelerometerSensor;
     private TrackingViewActivity trackingViewActivity;
     Accelerometer accSensor;
+    AccTriple accTriple;
+    private Boolean Flag;
     private boolean active;
     private int actualState;
+    private int countIndex;
 
     public SitUpsCtrl(TrackingViewActivity trackingViewActivity) {
         this.trackingViewActivity = trackingViewActivity;
@@ -29,29 +35,58 @@ public class SitUpsCtrl {
     public void init() {
         situpCount = 0;
         actualState = 0;
+        countIndex = 0;
         active = true;
 
         accSensor.init();
     }
 
-    public void calculateSitups() {
-        float prevValue = Float.MAX_VALUE;
+    public int calculateSitups() {
+        active = true;
+        AccTriple prevTriple = new AccTriple();
+        accTriple = trackingViewActivity.getAccelerometerQueue().get(countIndex);
         while (active) {
             switch (actualState) {
-                case 0: // start
+                case 0:
+                    prevTriple = accTriple;
+                    accTriple = trackingViewActivity.getAccelerometerQueue().get(countIndex++);
+
+                    if (accTriple != null) {
+
+                        // check for rising
+                        if (accTriple.getX() > prevTriple.getX() && accTriple.getY() > prevTriple.getY()) {
+                            actualState = 1;
+
+                        }
+                    } else {
+                        active = false;
+                    }
+                    // if rising found
+
                     break;
-                case 1: // found rising
-                    // if the current value in Array is < the previous change state to 2
+                case 1: // if the current value in Array is < the previous change state to 2
+                    prevTriple = accTriple;
+                    accTriple = trackingViewActivity.getAccelerometerQueue().get(countIndex++);
+
+                    if(accTriple != null) {
+                        if (accTriple.getX() < prevTriple.getX() && (accTriple.getY() < prevTriple.getY() && accTriple.getZ() < prevTriple.getZ())) {
+                            // found Falling
+                            actualState = 2;
+                        }
+                    } else {
+                        active = false;
+                    }
+
                     break;
                 case 2: // found falling -> peak found
                     // if change is found then make counter +
                     // change state to 0
+                    situpCount++;
+                    actualState = 0;
                     break;
             }
         }
-    }
-
-    public int getSitupCount() {
         return situpCount;
     }
+
 }
