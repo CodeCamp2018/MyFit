32d5a26863424aaf620378a8e9d8943829dab6a5
Gspin96 <higspin@gmail.com>
2018-08-03 12:20:19 +0200

    Added delete button (partial)

---
+++ app/src/main/java/com/uniks/myfit/helper/DeleteButtonHelper.java
@@ -0,0 +1,11 @@
+package com.uniks.myfit.helper;
+
+import android.view.View;
+
+public class DeleteButtonHelper implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        //TODO: open delete dialog
+    }
+}
+
